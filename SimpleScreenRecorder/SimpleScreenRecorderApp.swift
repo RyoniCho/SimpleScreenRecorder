979000303c46f7b85c7fbbfb55d8472f66555934
조을연 <carpinus@gmail.com>
2023-07-06 08:54:35 +0900

    Initial Commit

---
+++ SimpleScreenRecorder/SimpleScreenRecorderApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleScreenRecorderApp.swift
+//  SimpleScreenRecorder
+//
+//  Created by 조을연 on 2023/07/06.
+//
+
+import SwiftUI
+
+@main
+struct SimpleScreenRecorderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
